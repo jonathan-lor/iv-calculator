05ed09247c3993f6a8c7db976e4e70fa16fb22bc
Jonathan Lor <jonathan.r.lor@gmail.com>
2022-09-16 18:04:45 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+import Pokedex from 'pokedex-promise-v2';
+const P = new Pokedex();
+
+P.getPokemonByName("butterfree")
+    .then((response) => {
+        //0 is hp
+        console.log(response.stats[0].base_stat);
+    })
+    .catch((error) => {
+        console.log('There was an ERROR: ', error);
+    });
+*/
